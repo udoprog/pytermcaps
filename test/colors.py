a0662ec3dedce0f5ca4ceeed434bc03e70b1e40c
udoprog <johnjohn.tedro@gmail.com>
2010-03-16 12:02:24 +0100

    Added nice tests : D

--- test/colors.py
+++ test/colors.py
@@ -1,9 +1,18 @@
+# -*- encoding: utf-8 -*-
+
 from pytermcaps import TermCaps
 
 class TestTermCaps(TermCaps):
+    def error(self, desc, *args):
+        self._writeall(self.c.red, self.c.bold, self._encode(desc), self.c.sgr0, ": ", self.c.red, self._join(args), self.c.sgr0, "\n");
+
     def prettyprint(self, *args):
         self._writeall(self.c.green, self.c.bold, self._join(args), self.c.sgr0, "\n");
 
 tc = TestTermCaps();
 
+
 tc.prettyprint("Google", "is", "your", "friend");
+
+errorstring="Api failure";
+tc.error("An error occured", errorstring);
